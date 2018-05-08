8ccf8d165cdc1ed819915d5101510ec6ab95c389
logiclogue <jordanpaullord@hotmail.co.uk>
2018-05-08 09:48:27 +0100

    Adds drawing to the screen

---
+++ index.js
@@ -0,0 +1,8 @@
+(function () {
+    var canvas = document.getElementById("my-canvas");
+    var ctx = canvas.getContext("2d");
+
+    ctx.moveTo(0, 0);
+    ctx.lineTo(200, 200);
+    ctx.stroke();
+}());
