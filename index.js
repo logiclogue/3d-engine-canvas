fa6abb82230077d66902cba3a5f10dec357c921c
logiclogue <jordanpaullord@hotmail.co.uk>
2018-05-08 09:57:43 +0100

    Adds point

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var numjs = require("numjs");
 
+function Point(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+function pointFromArray(array) {
+    return new Point(array[0], array[1]);
+}
+
+function drawPoint(ctx, point) {
+    ctx.moveTo(point.x, point.y);
+    ctx.lineTo(point.x + 5, point.y + 5);
+    ctx.stroke();
+}
+
 (function () {
     var canvas = document.getElementById("my-canvas");
     var ctx = canvas.getContext("2d");
 
-    ctx.moveTo(0, 0);
-    ctx.lineTo(200, 200);
-    ctx.stroke();
+    drawPoint(ctx, new Point(10, 10));
 }());
