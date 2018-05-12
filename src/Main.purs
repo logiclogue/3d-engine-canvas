663465cfc2ec650930359520432b43cc08054e18
logiclogue <jordanpaullord@hotmail.co.uk>
2018-05-12 12:36:52 +0100

    Updates to draw rect to canvas

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,24 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+import Control.Monad.Eff (Eff)
+import Data.Maybe (Maybe(..))
+import Graphics.Canvas (
+    CANVAS, rect, fillPath, setFillStyle, getContext2D, getCanvasElementById)
+import Partial.Unsafe (unsafePartial)
+
+main :: Eff (canvas :: CANVAS) Unit
+main = void $ unsafePartial do
+    Just canvas <- getCanvasElementById "my-canvas"
+    ctx <- getContext2D canvas
+
+    _ <- setFillStyle "#0000FF" ctx
+
+    fillPath ctx $ rect ctx
+        {
+            x: 250.0,
+            y: 250.0,
+            w: 100.0,
+            h: 100.0
+        }
