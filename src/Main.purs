aa85345ec05e02e051593c31f6008894e70d7fac
logiclogue <jordanpaullord@hotmail.co.uk>
2018-05-15 08:26:12 +0100

    Updates to get requestanimationframe working

--- src/Main.purs
+++ src/Main.purs
@@ -7,20 +7,35 @@ import Data.Maybe (Maybe(..))
 import Graphics.Canvas (
     CANVAS, rect, fillPath, setFillStyle, getContext2D, getCanvasElementById)
 import Partial.Unsafe (unsafePartial)
+import DOM (DOM)
 import DOM.HTML (window)
 import DOM.HTML.Window (requestAnimationFrame)
+import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: Eff (canvas :: CANVAS) Unit
+main :: Eff (canvas :: CANVAS, console :: CONSOLE, dom :: DOM) Unit
 main = void $ unsafePartial do
     Just canvas <- getCanvasElementById "my-canvas"
     ctx <- getContext2D canvas
 
     _ <- setFillStyle "#0000FF" ctx
 
-    fillPath ctx $ rect ctx
+    _ <- fillPath ctx $ rect ctx
         {
             x: 250.0,
             y: 250.0,
             w: 100.0,
             h: 100.0
         }
+
+    _ <- log "HERE"
+
+    win <- window
+
+    requestAnimationFrame main win
+
+--
+--    requestAnimationFrame tick window
+--
+--tick :: forall eff. Eff (canvas :: CANVAS | eff) Unit
+--tick = do
+--    requestAnimationFrame tick window
