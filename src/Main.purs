fd3754da9a0c865f8ad616c65e47ad873b6d8724
logiclogue <jordanpaullord@hotmail.co.uk>
2018-05-15 21:05:34 +0100

    Updates to move the square across the screen

--- src/Main.purs
+++ src/Main.purs
@@ -5,12 +5,14 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Data.Maybe (Maybe(..))
 import Graphics.Canvas (
-    CANVAS, rect, fillPath, setFillStyle, getContext2D, getCanvasElementById)
+    CANVAS, Context2D, rect, fillPath, setFillStyle, getContext2D,
+    getCanvasElementById)
 import Partial.Unsafe (unsafePartial)
 import DOM (DOM)
 import DOM.HTML (window)
 import DOM.HTML.Window (requestAnimationFrame)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Data.Int (toNumber)
 
 main :: Eff (canvas :: CANVAS, console :: CONSOLE, dom :: DOM) Unit
 main = void $ unsafePartial do
@@ -19,23 +21,18 @@ main = void $ unsafePartial do
 
     _ <- setFillStyle "#0000FF" ctx
 
+    tick ctx 0
+
+tick :: forall eff. Context2D -> Int -> Eff (canvas :: CANVAS, dom :: DOM | eff) Unit
+tick ctx x = void do
     _ <- fillPath ctx $ rect ctx
         {
-            x: 250.0,
+            x: (toNumber x),
             y: 250.0,
             w: 100.0,
             h: 100.0
         }
 
-    _ <- log "HERE"
-
     win <- window
 
-    requestAnimationFrame main win
-
---
---    requestAnimationFrame tick window
---
---tick :: forall eff. Eff (canvas :: CANVAS | eff) Unit
---tick = do
---    requestAnimationFrame tick window
+    requestAnimationFrame (tick ctx (x + 1)) win
