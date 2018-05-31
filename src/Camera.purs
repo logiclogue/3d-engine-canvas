a0d318d7776459637028540e1e7fdc8a74bac42a
logiclogue <jordanpaullord@hotmail.co.uk>
2018-05-31 07:36:42 +0100

    Fixes last

--- src/Camera.purs
+++ src/Camera.purs
@@ -1,10 +1,8 @@
 module Camera where
 
-import Prelude
-
 import LinearAlgebra.Vector (Vector)
 
 type Camera = {
-    position :: Vector Number
+    position :: Vector Number,
     rotation :: Vector Number
 }
