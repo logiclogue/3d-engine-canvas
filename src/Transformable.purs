6a2e4dfe5260762d146170c8f62dab9fca71e700
logiclogue <jordanpaullord@hotmail.co.uk>
2018-05-31 07:56:27 +0100

    Adds transformable class

---
+++ src/Transformable.purs
@@ -0,0 +1,10 @@
+module Transformable where
+
+import LinearAlgebra.Vector (Vector)
+
+class Transformable a where
+    rotateX :: Number -> a -> a
+    rotateY :: Number -> a -> a
+    rotateZ :: Number -> a -> a
+    scale :: Number -> a -> a
+    shift :: Vector Number -> a -> a
