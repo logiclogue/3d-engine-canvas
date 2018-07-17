85bf78b586daf72a28d67e3950248d9c533e3689
logiclogue <jordanpaullord@hotmail.co.uk>
2018-07-17 15:39:32 +0100

    Fixes last, still errors

--- src/MatrixFunctions.purs
+++ src/MatrixFunctions.purs
@@ -1,22 +1,24 @@
 module MatrixFunctions where
 
+import Prelude (($))
+
 import TransformationMatrices (xRotationMatrix, yRotationMatrix, zRotationMatrix, scaleMatrix)
 import MatrixHelpers (mapColumns)
 import LinearAlgebra.Matrix (Matrix, multiply)
 import LinearAlgebra.Vector (add) as Vector
 import LinearAlgebra.Vector (Vector)
 import Transformable (class Transformable)
-import Data.Newtype (class Newtype)
+import Data.Newtype (class Newtype, wrap, unwrap)
 
-newtype MyMatrix = MyMatrix Number
+newtype MyMatrix = MyMatrix (Matrix Number)
 
-instance newtypeMyMatrix :: Newtype (MyMatrix) where
-    wrap (Matrix x) = MyMatrix x
-    unwrap (MyMatrix x) = Matrix x
+instance newtypeMyMatrix :: Newtype MyMatrix (Matrix Number) where
+    wrap matrix = MyMatrix matrix
+    unwrap (MyMatrix matrix) = matrix
 
 instance transformableMatrix :: Transformable (MyMatrix) where
-    rotateX angle m = xRotationMatrix angle `multiply` m
-    rotateY angle m = yRotationMatrix angle `multiply` m
-    rotateZ angle m = zRotationMatrix angle `multiply` m
-    scale factor m = scaleMatrix factor `multiply` m
-    shift vector m = mapColumns (\v -> Vector.add v vector) m
+    rotateX angle m = wrap $ xRotationMatrix angle `multiply` (unwrap m)
+    rotateY angle m = wrap $ yRotationMatrix angle `multiply` (unwrap m)
+    rotateZ angle m = wrap $ zRotationMatrix angle `multiply` (unwrap m)
+    scale factor m = wrap $ scaleMatrix factor `multiply` (unwrap m)
+    shift vector m = wrap $ mapColumns (\v -> Vector.add v vector) (unwrap m)
